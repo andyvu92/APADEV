632fdfee84f2764ac68960c426d9afdb75875306
PHYSIOTHERAPY\jhu <Jing.Hu@physiotherapy.asn.au>
2018-01-23 14:12:51 +1100

    Group Edit and Create

--- themes/evolve/Survey/SurveyEdit.inc.php
+++ themes/evolve/Survey/SurveyEdit.inc.php
@@ -1,4 +1,110 @@
-<?php
-/* Survey Group Edit/Submit */
-
-?>
+<?php if(!isset($_GET["GID"])):?>
+<form method="POST" action="../sites/all/themes/evolve/Survey/Surveydatabase.inc.php?action=create">
+   <div class="row">
+        <div class="col-lg-6">
+            <label for="">Group Title</label>
+            <input type="text" class="form-control" name="groupTitle">
+        </div>
+        <div class="col-lg-6">
+            <label for="">Group Description</label>
+            <input type="text" class="form-control" name="groupDescription">
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-6">
+            <label for="">Group StartDate</label>
+            <input type="date" class="form-control" name="groupStartDate">
+        </div>
+        <div class="col-lg-6">
+            <label for="">Group EndDate</label>
+            <input type="date" class="form-control" name="groupEndDate">
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-6">
+            <label for="">User Number</label>
+            <input type="text" class="form-control" name="userNumber">
+        </div>
+        <div class="col-lg-6">
+            <label for="">User Type</label>
+            <input type="text" class="form-control" name="userType">
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-6">
+            <label for="">Published Status</label>
+            <select class="form-control" id="publishStatus" name="publishStatus">
+                              <option value="1">YES</option>
+                              <option value="0">NO</option>
+            </select>
+        </div>
+        <div class="col-lg-6"><button type="submit">Save</button></div>
+    </div>
+</form>
+<?php endif;?>
+<?php if(isset($_GET["GID"])):?>
+ <?php    if(isset($_GET["GID"])){
+	   $GID = $_GET['GID'];
+	   $dbt = new PDO('mysql:host=localhost;dbname=apa_extrainformation', 'c0DefaultMain', 'Apa2017Config');
+	   $connSelect = $dbt->prepare('SELECT * FROM groups WHERE GroupID= :GID');
+	   $connSelect->bindValue(':GID', $GID);
+	   $connSelect->execute();
+	if($connSelect->rowCount()>0) { 
+      foreach ($connSelect as $row) {
+		$parentID= $row['ParentID'];
+		$GTitle = $row['GroupTitle'];
+		$GDescription = $row['groupDescription'];
+		$GStartDate = $row['groupStartDate'];
+		$GEndDate = $row['groupEndDate'];
+		$GUserNum = $row['GroupNumUserLimit'];
+		$GUserType = $row['GroupUserTypeLimit'];
+		$GStatus = $row['IsPublished'];
+	          break;}
+	}	
+	    $connSelect = null;
+ }
+ ?>
+ <form method="POST" action="../sites/all/themes/evolve/Survey/Surveydatabase.inc.php?action=edit">
+   <div class="row">
+        <div class="col-lg-6">
+            <label for="">Group Title</label>
+			<input type="hidden" name="GID" value="<?php echo $GID;?>">
+            <input type="text" class="form-control" name="groupTitle" value="<?php echo $GTitle;?>">
+        </div>
+        <div class="col-lg-6">
+            <label for="">Group Description</label>
+            <input type="text" class="form-control" name="groupDescription" value="<?php echo $GDescription;?>">
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-6">
+            <label for="">Group StartDate</label>
+            <input type="date" class="form-control" name="groupStartDate" value="<?php echo $GStartDate;?>">
+        </div>
+        <div class="col-lg-6">
+            <label for="">Group EndDate</label>
+            <input type="date" class="form-control" name="groupEndDate" value="<?php echo $GEndDate;?>">
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-6">
+            <label for="">User Number</label>
+            <input type="text" class="form-control" name="userNumber" value="<?php echo $GUserNum;?>">
+        </div>
+        <div class="col-lg-6">
+            <label for="">User Type</label>
+            <input type="text" class="form-control" name="userType" value="<?php echo $GUserType;?>">
+        </div>
+    </div>
+	<div class="row">
+        <div class="col-lg-6">
+            <label for="">Published Status</label>
+            <select class="form-control" id="publishStatus" name="publishStatus">
+                              <option <?php if($GStatus =="1") echo "selected";?> value="1">YES</option>
+                              <option <?php if($GStatus =="0") echo "selected";?> value="0">NO</option>
+            </select>
+        </div>
+        <div class="col-lg-6"><button type="submit">Save</button></div>
+    </div>
+</form>
+<?php endif;?>
--- themes/evolve/Survey/Surveydatabase.inc.php
+++ themes/evolve/Survey/Surveydatabase.inc.php
@@ -1,4 +1,57 @@
 <?php
 /* Survey Database */
+/* Survey Group Edit/Create */
+
+$dbt = new PDO('mysql:host=localhost;dbname=apa_extrainformation', 'c0DefaultMain', 'Apa2017Config'); 
+
+
+$GDescription = '';
+$GStartDate = '';
+$GEndDate = '';
+$userNumber = '';
+$userType = '';
+$publishStatus = '';
+ if(isset($_GET["action"])&& $_GET["action"]=="create"){
+      $GTitle = $_POST['groupTitle'];
+	   if(isset($_POST['groupDescription'])){ $GDescription = $_POST['groupDescription']; }
+	   if(isset($_POST['groupStartDate'])){ $GStartDate = $_POST['groupStartDate']; }
+       if(isset($_POST['groupEndDate'])){ $GEndDate = $_POST['groupEndDate']; }    
+	   if(isset($_POST['userNumber'])){ $GUserNum = $_POST['userNumber']; } 
+	   if(isset($_POST['userType'])){ $GUserType = $_POST['userType']; }
+       if(isset($_POST['publishStatus'])){ $GStatus = $_POST['publishStatus']; } 	   
+          $connCreate= $dbt->prepare('INSERT INTO groups (GroupTitle, GroupDescription, GroupStartDate, GroupEndDate, GroupNumUserLimit, GroupUserTypeLimit, IsPublished) VALUES (:GTitle, :GDescription, :GStartDate, :GEndDate, :userNumber, :userType, :publishStatus)');
+		  $connCreate->bindValue(':GTitle', $GTitle);
+          $connCreate->bindValue(':GDescription', $GDescription);
+		  $connCreate->bindValue(':GStartDate', $GStartDate);
+		  $connCreate->bindValue(':GEndDate', $GEndDate);
+		  $connCreate->bindValue(':userNumber', $userNumber);
+		  $connCreate->bindValue(':userType', $userType);
+		  $connCreate->bindValue(':publishStatus', $publishStatus);
+		  $connCreate->execute();
+          $connCreate= null;
+     echo "save sccussfully!";
+    }
+ 
+ if(isset($_GET["action"])&&$_GET["action"]=="edit"){
+	   $GID = $_POST['GID'];
+       $GTitle = $_POST['groupTitle'];
+	   if(isset($_POST['groupDescription'])){ $GDescription = $_POST['groupDescription']; }
+	   if(isset($_POST['groupStartDate'])){ $GStartDate = $_POST['groupStartDate']; }
+       if(isset($_POST['groupEndDate'])){ $GEndDate = $_POST['groupEndDate']; }    
+	   if(isset($_POST['userNumber'])){ $GUserNum = $_POST['userNumber']; } 
+	   if(isset($_POST['userType'])){ $GUserType = $_POST['userType']; }
+       if(isset($_POST['publishStatus'])){ $GStatus = $_POST['publishStatus']; } 
+          $connUpdate= $dbt->prepare('UPDATE groups SET GroupTitle = :GTitle, GroupDescription = :GDescription, GroupStartDate = :GStartDate, GroupEndDate = :GEndDate, GroupNumUserLimit = :userNumber, GroupUserTypeLimit = :userType, IsPublished = :publishStatus WHERE GroupID= :GID');
+		  $connUpdate->bindValue(':GTitle', $GTitle);
+          $connUpdate->bindValue(':GDescription', $GDescription);
+		  $connUpdate->bindValue(':GStartDate', $GStartDate);
+		  $connUpdate->bindValue(':GEndDate', $GEndDate);
+		  $connUpdate->bindValue(':userNumber', $userNumber);
+		  $connUpdate->bindValue(':userType', $userType);
+		  $connUpdate->bindValue(':publishStatus', $publishStatus);
+		  $connUpdate->execute();
+          $connUpdate= null;
+        echo "save sccussfully!"; 
+    }	
 
 ?>
