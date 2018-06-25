4aea8c2c23dbb72334f369f27d2dda1c5a8bc411
PHYSIOTHERAPY\ekim <eddy.kim@physiotherapy.asn.au>
2018-06-25 12:57:40 +1000

    log file change

--- themes/evolve/inc/Aptify/JSONarray.inc.php
+++ themes/evolve/inc/Aptify/JSONarray.inc.php
@@ -51,4 +51,121 @@ function json_clean_decode($json, $assoc = false, $depth = 512, $options = 0) {
 	$json = json_decode($json, $assoc);
 	return $json;
 }
+
+function logTransaction($APINum, $Sent, $Got) {
+	$sizeByte = intval(filesize("sites/Log/APA_Aptify_Communication.log"));
+	$size = FileSizeConvert($sizeByte);
+	//echo "size: ".$size." // ".filesize("sites/Log/APA_Aptify_Communication.log")."<br />";
+	if($sizeByte > 100000) {
+		fileloop();
+	}
+	$fileContinue = "";
+	if(file_exists("sites/Log/APA_Aptify_Communication.log")){ // Check If File Already Exists
+		$myfilet = fopen("sites/Log/APA_Aptify_Communication.log", "r");
+		$fileContinue = fread($myfilet,filesize("sites/Log/APA_Aptify_Communication.log"));
+		//echo "Yo: ".$fileContinue."!<br />";
+		fclose($myfilet);
+	}
+	//echo "<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>";
+	//echo $fileContinue;
+	//echo "<br>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<br>";
+	$myfile = fopen("sites/Log/APA_Aptify_Communication.log", "w");
+	// log:
+	// UserID, Date/Time, Data Sent,
+	// Data Received, Web service Number
+	fwrite($myfile, $fileContinue);
+	$txt = "UserID: ";
+	if(isset($_SESSION["UserId"])) {$txt .= $_SESSION["UserId"]."\n";} else {$txt .= "Null\n";}
+	$txt .= "Date/time: ".date("Y-m-d h-i-s")."\n";
+	$txt .= "Web Service No: ".$APINum."\n";
+	$txt .= "Date Sent: \n";
+	$txt .= $Sent."\n";
+	$txt .= "Data Received: \n";
+	$txt .= $Got."\n";
+	$txt .= "---End of Log (".date("Y-m-d h-i-s").")---\n\n\n\n\n";
+	//echo $txt;
+	fwrite($myfile, $fileContinue.$txt);
+	fclose($myfile);
+	// log file output.
+}
+
+// push file names' number increased by 1.
+function fileloop() {
+	$num = count(scandir('sites/Log/')) - 2;
+	$arrayT = array();
+	if($handle = opendir('sites/Log/')) {
+		while (false !== ($fileName = readdir($handle))) {
+			array_push($arrayT, $fileName);			
+		}
+		closedir($handle);
+	}
+	arsort($arrayT);
+	foreach($arrayT as $fileName) {
+		if(strlen($fileName) > 2 ) {
+			//echo $fileName." - iiiinnnnn!!!<br />";
+			if($fileName == "APA_Aptify_Communication.log") {
+					$newName = $newName = str_replace("APA_Aptify_Communication.log","APA_Aptify_Communication1.log",$fileName);
+				} else {
+					$newName = str_replace((string)($num - 1),(string)$num,$fileName);
+				}
+				$num--;
+				//echo "new Name: ".$newName."<br />";
+			if(!file_exists("sites/Log/".$newName)){ // Check If File Already Exists
+				if(rename("sites/Log/".$fileName, "sites/Log/".$newName)){ // Check If rename Function Completed Successfully
+					//echo "Successfully Renamed $fileName to $newName<br />" ;
+				}else{
+					//echo "There Was Some Error While Renaming $fileName<br />" ;
+				}
+			}else{
+				//echo "A File With The New File Name Already Exists<br />" ;
+			}
+		}
+	}
+}
+
+
+/** 
+* Converts bytes into human readable file size. 
+* 
+* @param string $bytes 
+* @return string human readable file size (2,87 Мб)
+* @author Mogilev Arseny 
+*/
+function FileSizeConvert($bytes)
+{
+    $bytes = floatval($bytes);
+        $arBytes = array(
+            0 => array(
+                "UNIT" => "TB",
+                "VALUE" => pow(1024, 4)
+            ),
+            1 => array(
+                "UNIT" => "GB",
+                "VALUE" => pow(1024, 3)
+            ),
+            2 => array(
+                "UNIT" => "MB",
+                "VALUE" => pow(1024, 2)
+            ),
+            3 => array(
+                "UNIT" => "KB",
+                "VALUE" => 1024
+            ),
+            4 => array(
+                "UNIT" => "B",
+                "VALUE" => 1
+            ),
+        );
+
+    foreach($arBytes as $arItem)
+    {
+        if($bytes >= $arItem["VALUE"])
+        {
+            $result = $bytes / $arItem["VALUE"];
+            $result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
+            break;
+        }
+    }
+    return $result;
+}
 ?>
