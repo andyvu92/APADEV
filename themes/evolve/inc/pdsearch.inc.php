9ff2a341d220172c6780cf02a8618ecd8721adee
PHYSIOTHERAPY\wdeveloper <web.dev@physiotherapy.asn.au>
2018-07-20 09:42:07 +1000

    Fix PD Search auto scroll

--- themes/evolve/inc/pdsearch.inc.php
+++ themes/evolve/inc/pdsearch.inc.php
@@ -182,7 +182,7 @@
 		$("#block-block-241").addClass("display");
 		$("#section-parallax-first").removeClass("display-none");
 		$("#section-parallax-first").addClass("display");
-		$('html,body').animate({ scrollTop: $('#section-content-top').position().top  - $('#section-header').height() }, 1000);
+		$('html,body').animate({ scrollTop: $('#section-content-top').offset().top  - $('#section-header').height() }, 1000);
 	}
                     
 });
