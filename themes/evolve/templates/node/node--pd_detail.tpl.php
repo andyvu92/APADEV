5a708531dc94f2e843baf52a14d93c0daa845a68
PHYSIOTHERAPY\jhu <Jing.Hu@physiotherapy.asn.au>
2018-09-13 11:15:36 +1000

    Added more error message when adding payment card failed in PD shopping cart page.

--- themes/evolve/templates/node/node--pd_detail.tpl.php
+++ themes/evolve/templates/node/node--pd_detail.tpl.php
@@ -1548,7 +1548,7 @@ $userRetisterStatus = false;
 							
 						}
 						else{
-							$('#checkMessage').html("");
+							$('#checkMessage').addClass("display-none");
 							$( "#Memberid" ).blur();
 							$("#Memberid").css("border", "");
 							//$(".join-details-button2").removeClass("display-none");
@@ -1607,18 +1607,18 @@ $userRetisterStatus = false;
 							$('#PasswordMessage').html("8 characters minimum");
 							$( "#newPassword" ).focus();
 							$("#newPassword").css("border", "1px solid #ffa02e");
-							$(".join-details-button2").addClass("display-none");
-						}else if($( "#Password" ).val().length <= 7){
+							$(".accent-btn").addClass("stop");
+						}else if($( "#Password" ).val()!="" && $( "#Password" ).val().length <= 7){
 							$('#checkPasswordMessage').html("8 characters minimum");
 							$( "#Password" ).focus();
 							$("#Password").addClass('focuscss');
-							$(".join-details-button2").addClass("display-none");
+							$(".accent-btn").addClass("stop");
 						}
 						else{
 							$('#PasswordMessage').html("");
 							
 							$("#newPassword").css("border", "");
-							$(".join-details-button2").removeClass("display-none");
+							$(".accent-btn").removeClass("stop");
 						}					
 					}
 				   	function checkPasswordFunction(Password) {
@@ -1626,19 +1626,19 @@ $userRetisterStatus = false;
 							$('#checkPasswordMessage').html("Your passwords do not match");
 							$( "#Password" ).focus();
 							$("#Password").css("border", "1px solid #ffa02e");
-							$(".join-details-button2").addClass("display-none");
+							$(".accent-btn").addClass("stop");
 							
 						}else if($( "#Password" ).val().length <= 7){
 							$('#checkPasswordMessage').html("8 characters minimum");
 							$( "#Password" ).focus();
 							$("#Password").addClass('focuscss');
-							$(".join-details-button2").addClass("display-none");
+							$(".accent-btn").addClass("stop");
 						}
 						else{
 							$('#checkPasswordMessage').html("");
 							
 							$("#Password").css("border", "");
-							$(".join-details-button2").removeClass("display-none");
+							$(".accent-btn").removeClass("stop");
 						}					
 					}
 				</script>
