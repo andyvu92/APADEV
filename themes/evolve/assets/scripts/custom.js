d3f5d9967b7e630f82de5453e8d2ca5160674a3a
PHYSIOTHERAPY\ekim <eddy.kim@physiotherapy.asn.au>
2018-09-19 16:05:48 +1000

    no message

--- themes/evolve/assets/scripts/custom.js
+++ themes/evolve/assets/scripts/custom.js
@@ -1114,7 +1114,7 @@ jQuery(document).ready(function($) {
 		window.history.back();
 	}
     BringSurveyBack();
-    if(x == 2 || x == 3) {
+    if(x == 1 || x == 2) {
       $(".node-membership-type").hide();
       $('[id^=question]').hide();
       $('[id^=question]').addClass("function");
