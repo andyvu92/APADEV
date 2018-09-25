b18234c840bb0aee96bb5c55bae915a16968e120
PHYSIOTHERAPY\jhu <Jing.Hu@physiotherapy.asn.au>
2018-09-25 13:18:30 +1000

    About us page team content type

--- modules/custom_modules/apa_function/inc/renewMyMembership/renew-yourdetail.inc.php
+++ modules/custom_modules/apa_function/inc/renewMyMembership/renew-yourdetail.inc.php
@@ -1468,10 +1468,10 @@ if (!empty($details['Regionalgp'])) { $_SESSION['Regional-group'] = $details['Re
                                 <select class="form-control" name="Udegree0" id="Udegree0">
                                    <option value="" selected disabled>Please select</option>
 								   <?php
-                                        /*$degreecode         = file_get_contents("sites/all/themes/evolve/json/Educationdegree.json");
+                                        $degreecode         = file_get_contents("sites/all/themes/evolve/json/Educationdegree.json");
                                         $degree             = json_decode($degreecode, true);
                                         $_SESSION["degree"] = $degree;
-                                        foreach ($degree as $pair => $value) {
+                                        /*foreach ($degree as $pair => $value) {
                                             echo '<option value="' . $degree[$pair]['ID'] . '"';
                                             echo '> ' . $degree[$pair]['Name'] . ' </option>';
                                         }*/
