1cb6d3dbc51493c21fb1bd6c9ca92476ec13305f
PHYSIOTHERAPY\ekim <eddy.kim@physiotherapy.asn.au>
2018-10-10 12:32:10 +1100

    Google API

--- themes/evolve/templates/node/node--pd_detail.tpl.php
+++ themes/evolve/templates/node/node--pd_detail.tpl.php
@@ -749,7 +749,7 @@ $userRetisterStatus = false;
   width="600"
   height="450"
   frameborder="0" style="border:0"
-  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAkdk-DPQmLPHReK_uWEbWXn2XJGOp6fto
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBUXY9mb7uoQp8PtmLH8tNkLvr7Vdm6xAQ
     &q='.$pd_detail['AddressLine1']." ".$pd_detail['AddressLine2']." ".$pd_detail['City']." ".$pd_detail['State']." ".$pd_detail['PostalCode'].'" allowfullscreen>
 </iframe>';
    
