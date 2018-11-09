410273976df8bba882dc067b7ba500135aba58a8
PHYSIOTHERAPY\jhu <Jing.Hu@physiotherapy.asn.au>
2018-11-09 11:52:01 +1100

    remove the donate PRF from the research article sider

--- themes/evolve/templates/node/node--research_article.tpl.php
+++ themes/evolve/templates/node/node--research_article.tpl.php
@@ -175,13 +175,7 @@
                   $output = drupal_render($get);        
                   print $output;
             ?>
-            <?php 
-                  $block = block_load('block', '224');
-                  $get = _block_get_renderable_array(_block_render_blocks(array($block)));
-                  $output = drupal_render($get);        
-                  print $output;
-            
-            ?>
+          
             <?php 
                   $block = block_load('block', '225');
                   $get = _block_get_renderable_array(_block_render_blocks(array($block)));
