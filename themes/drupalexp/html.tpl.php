7cfc6506dbda0ad3b95c6e7a8e02e2d7ff9bcb52
PHYSIOTHERAPY\jhu <Jing.Hu@physiotherapy.asn.au>
2018-11-28 13:59:05 +1100

    Fixed select broken issue (Tomorrow confirm with Andy new js file )

--- themes/drupalexp/html.tpl.php
+++ themes/drupalexp/html.tpl.php
@@ -5,11 +5,11 @@
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="/sites/all/themes/evolve/assets/scripts/typeahead.js-master/dist/typeahead.bundle.min.js" type="text/javascript"></script>
 
-	<script src="//code.jquery.com/mobile/1.5.0-alpha.1/jquery.mobile-1.5.0-alpha.1.min.js"></script>
+	
 
 	<!-- SELECTIZE -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.js"></script>
-
+	
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.1/css/selectize.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.bootstrap3.min.css">
 
@@ -89,7 +89,7 @@
 
 		gtag('config', 'UA-32728465-9');
 		</script>
-
+    <script src="//code.jquery.com/mobile/1.5.0-alpha.1/jquery.mobile-1.5.0-alpha.1.min.js"></script>
 	</head>
 	<?php
 	// variables changed TODO
@@ -135,5 +135,6 @@
 			window.location = locationC;
 		}); 
 		</script>
+		
 	</body>
 </html>
