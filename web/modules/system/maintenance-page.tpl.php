450b24f035142ce8307f2ebbe8b6f8fd492f0214
Eddy Kim <Eddy.Kim@physiotherapy.asn.au>
2019-04-10 22:49:27 +1000

    Maintenance page fix & .gitignore update

--- web/modules/system/maintenance-page.tpl.php
+++ web/modules/system/maintenance-page.tpl.php
@@ -13,40 +13,32 @@
  * @ingroup themeable
  */
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
+
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cairo:regular|Roboto:100,300,700&amp;subset=latin,greek,cyrillic" media="all">
+  <style>	.background {
+	    position: fixed; overflow: hidden;background-image: url('/sites/default/files/PARALLAX_HOME.jpg');background-size: cover;background-repeat:  no-repeat;left:  0;top:  0;margin: 0;background-position:  center; right: 0; bottom: 0;
+    }
+    .background:before {
+        content: ''; height: 100%; position: absolute; width: 100%; background-color: rgba(0,0,0,0.3); z-index: 2; top: 0; left: 0; bottom: 0; right: 0;
+    }
+</style>
 </head>
 <body class="<?php print $classes; ?>">
   <div id="page">
     <div id="header">
-      <div id="logo-title">
-
-        <?php if (!empty($logo)): ?>
-          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-          </a>
-        <?php endif; ?>
-
-        <div id="name-and-slogan">
-          <?php if (!empty($site_name)): ?>
-            <h1 id="site-name">
-              <a href="<?php print $base_path ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-
-          <?php if (!empty($site_slogan)): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-        </div> <!-- /name-and-slogan -->
-      </div> <!-- /logo-title -->
-
+      <?php /*
+	  <div id="logo-title">
+		*/ ?>
       <?php if (!empty($header)): ?>
         <div id="header-region">
           <?php print $header; ?>
@@ -64,12 +56,28 @@
       <?php endif; ?>
 
       <div id="main" class="column"><div id="main-squeeze">
-
-        <div id="content">
-          <?php if (!empty($title)): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <div class="background">&nbsp;</div>
+        <div id="content" style="padding: 50px; position: relative; z-index: 5;">
+          <?php /*if (!empty($title)): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif;*/ ?>
           <?php if (!empty($messages)): print $messages; endif; ?>
-          <div id="content-content" class="clearfix">
-            <?php print $content; ?>
+          <div id="content-content" class="clearfix" style="font-size: 45px;font-family: arial;color: rgb(0,159,218);">
+		  
+		  <div id="logo-title">
+			  <?php /*<a href="<?php //print $base_path; ?>https://choose.physio" title="<?php print t('Home'); ?>" rel="home" id="logo">*/ ?>
+        <img alt="" src="/sites/default/files/APA_185x2242.png">
+			  <?php /* </a> */ ?>
+		  </div> <!-- /logo-title -->
+		  
+          <?php /*print $content;*echo "This website is currently undergoing maintenance.<br />We apologies for any inconvenience, please come back soon.";*/ ?>
+            <div style="width: 56%; margin: auto;">
+              <p style="margin-top: 0; margin-bottom: 50px; font-weight: 700; color: white; font-family: 'roboto';">
+              This website is presently unavailable while we undergo maintenance.<br />Please come back soon
+        or phone the APA Member Hub on 1300 306 622.
+              </p>
+              <p style="margin-top: 0; font-weight: 100; font-size: 36px; font-family: 'roboto'; color: white;">
+                
+              </p>
+            </div>
           </div> <!-- /content-content -->
         </div> <!-- /content -->
 
