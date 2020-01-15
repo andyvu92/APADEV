6fdc147bc494405f53e5a8c6ccc367c3519e4b6a
PHYSIOTHERAPY\ekim <eddy.kim@physiotherapy.asn.au>
2020-01-16 09:16:58 +1100

    Home news contents - Access directly, only via admin.

--- web/sites/all/themes/evolve/templates/node/node--home_news_tiles.tpl.php
+++ web/sites/all/themes/evolve/templates/node/node--home_news_tiles.tpl.php
@@ -80,30 +80,14 @@
    *
    * @ingroup themeable
    */
+  global $user;
+$userRole = $user->roles[3];
    ?>
 <div style="height: 10px; width: 1px;">&nbsp;</div>
-
-<div class="Pagination">
-
-</div> 
+<div class="Pagination"></div>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix post large" <?php print $attributes; ?>>
-   
+<?php if(is_null(render($content['field_url_for_page'])) && is_null(render($content['field_external_url_for_page']))): ?>
    <section class="post-content container" style="float:none; margin:auto;">
- 
-      <?php /*
-         use this header as reference, but delete at the end
-         <header class="meta">
-         	   <ul>
-                  <li><a href="#"><?php print t('Posted by'); ?><?php print theme('username', array('account' => $node)); ?></a></li>
-      <li><?php print render($content['field_media_type']);?></li>
-      <li><?php print date('M',$created); print " "; print date('d',$created).' '.date('Y',$created); ?></li>
-      </ul>
-      <h2><a href="<?php print $node_url; ?>">
-         <?php print $title; ?></a>
-      </h2>
-      </header>
-      */ //['field_emailauthor']['und'][0]['value']?>
-      <?php /*print_r ($content['field_research_author']['#items']); */?>	
       <div class="region col-xs-12 col-sm-12 col-md-8 col-lg-8">
 
         <!-- MOBILE POST FEATURED IMG -->
@@ -117,55 +101,52 @@
          <div class="brd-headling">&nbsp;</div>
 			
          <header class="meta">
-    <div class="flex-container post-meta">
+            <div class="flex-container post-meta">
 
-        <div class="flex-cell">
-            <div class="flex-col-6 meta-info">
-                        <?php 
-							$only = ((array)$content['field_members_only']['#items'][0]['taxonomy_term'])["name"];
-							if($only == "Yes") {
-								echo "<li><strong><div class='MonlyIconHolder'><div class='MonlyIcon'></div></div></strong></li>";
-							} 
-						?>
-						<?php if(!is_null(render($content['field_members_only']))): ?> 
-			    		<?php endif?>
-                <div class="meta-author-date">
-                    <!-- <span class="meta-author">By <b><?php //print render($content['field_inmotion_author']); ?></b></span> -->
-                    <span class="meta-date"><?php print date('M',$created); print " "; print date('d',$created).' '.date('Y',$created); ?></span>
-                </div>
+               <div class="flex-cell">
+                     <div class="flex-col-6 meta-info">
+                                 <?php 
+                              $only = ((array)$content['field_members_only']['#items'][0]['taxonomy_term'])["name"];
+                              if($only == "Yes") {
+                                 echo "<li><strong><div class='MonlyIconHolder'><div class='MonlyIcon'></div></div></strong></li>";
+                              } 
+                           ?>
+                           <?php if(!is_null(render($content['field_members_only']))): ?> 
+                           <?php endif?>
+                        <div class="meta-author-date">
+                           <!-- <span class="meta-author">By <b><?php //print render($content['field_inmotion_author']); ?></b></span> -->
+                           <span class="meta-date"><?php print date('M',$created); print " "; print date('d',$created).' '.date('Y',$created); ?></span>
+                        </div>
+                     </div>
+
+                     <div class="flex-col-6 meta-share-url">
+                        <ul class="socialMediaIcon" style="float:right;margin-top:-3%;">
+                        <li>
+                           <div class="fb-share-button" data-layout="button_count" data-mobile-iframe="true" data-size="small">&nbsp;</div>
+                        </li>
+                        <li>
+                           <a class="twitter-share-button" data-show-count="false" href="https://twitter.com/share">Tweet</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8">&nbsp;</script>
+                        </li>
+                        <li>
+                           <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script><script type="IN/Share" data-counter="right"></script>&nbsp;
+                        </li>
+                        <li>
+                           <!-- Place this tag where you want the share button to render. -->
+                           <div class="g-plus" data-action="share" data-annotation="none">&nbsp;</div>
+                        </li>
+                        </ul>
+                     </div>
+               </div>
+
             </div>
-
-            <div class="flex-col-6 meta-share-url">
-                <ul class="socialMediaIcon" style="float:right;margin-top:-3%;">
-					<li>
-					   <div class="fb-share-button" data-layout="button_count" data-mobile-iframe="true" data-size="small">&nbsp;</div>
-					</li>
-					<li>
-					   <a class="twitter-share-button" data-show-count="false" href="https://twitter.com/share">Tweet</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8">&nbsp;</script>
-					</li>
-					<li>
-					   <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script><script type="IN/Share" data-counter="right"></script>&nbsp;
-					</li>
-					<li>
-					   <!-- Place this tag where you want the share button to render. -->
-					   <div class="g-plus" data-action="share" data-annotation="none">&nbsp;</div>
-					</li>
-	         	</ul>
+         </header>
+         <!-- DESKTOP POST FEATURED IMG -->
+         <div class="post-img media">
+            <div class='mediaholder fullwidthimage'>
+               <?php print render($content['field_home_tile_image']);?>
             </div>
-        </div>
-
-    </div>
-</header>
-		 
-        
-         
-        <!-- DESKTOP POST FEATURED IMG -->
-		<div class="post-img media">
-			<div class='mediaholder fullwidthimage'>
-				<?php print render($content['field_home_tile_image']);?>
-			</div>
-		</div>
-		 <?php
+         </div>
+         <?php
             // We hide the comments and links now so that we can render them later.
             hide($content['comments']);
             hide($content['links']);
@@ -184,36 +165,135 @@
             }(document, 'script', 'facebook-jssdk'));
          </script><script type="text/javascript">
             jQuery(document).ready(function($) {
-				
-            var x = $(location).attr('href');
-            jQuery(document).ready(function($) {
-            	$('.fb-share-button').html("<a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=" +  x  +  "&amp;src=sdkpreparse'></a>")
-            });
-            
-            })
-              
+               var x = $(location).attr('href');
+               jQuery(document).ready(function($) {
+                  $('.fb-share-button').html("<a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=" +  x  +  "&amp;src=sdkpreparse'></a>")
+               });
+            })  
          </script>
-        
         <div id="prev-btn">
             <a class="go-back-button button" href="javascript:history.go(-1)">Back to previous</a>
         </div>
-	
       </div>
-	  
       <div class="block CampaignSidebar contextual-links-region region-right-sidebar col-xs-12 col-sm-12 col-md-4 col-lg-4" style="margin:0 0 50px">
          <h3 class="headline">Most Popular</h3>
          <span class="brd-headling"></span>
          <div class="content">
             <?php echo views_embed_view('home_tile_view', 'block_6');?>
-	
          </div>
-		
       </div>
-	
-	
-	
    </section>
 
-   <!---line-->
-   <!----line--->
+   <?php elseif(!is_null(render($content['field_url_for_page'])) || !is_null(render($content['field_external_url_for_page']))): ?>
+      <?php if($userRole == "administrator"): ?>
+         <section class="post-content container" style="float:none; margin:auto;">
+            <div class="region col-xs-12 col-sm-12 col-md-8 col-lg-8">
+      
+            <!-- MOBILE POST FEATURED IMG -->
+            <div class="post-img media mobile">
+                  <div class='mediaholder fullwidthimage'>
+                     <?php print render($content['field_home_tile_image']);?>
+                  </div>
+            </div>
+      
+               <h1 class="SectionHeader"><?php print $node->title;?></h1>
+               <div class="brd-headling">&nbsp;</div>
+               
+               <header class="meta">
+                  <div class="flex-container post-meta">
+      
+                     <div class="flex-cell">
+                           <div class="flex-col-6 meta-info">
+                                       <?php 
+                                    $only = ((array)$content['field_members_only']['#items'][0]['taxonomy_term'])["name"];
+                                    if($only == "Yes") {
+                                       echo "<li><strong><div class='MonlyIconHolder'><div class='MonlyIcon'></div></div></strong></li>";
+                                    } 
+                                 ?>
+                                 <?php if(!is_null(render($content['field_members_only']))): ?> 
+                                 <?php endif?>
+                              <div class="meta-author-date">
+                                 <!-- <span class="meta-author">By <b><?php //print render($content['field_inmotion_author']); ?></b></span> -->
+                                 <span class="meta-date"><?php print date('M',$created); print " "; print date('d',$created).' '.date('Y',$created); ?></span>
+                              </div>
+                           </div>
+      
+                           <div class="flex-col-6 meta-share-url">
+                              <ul class="socialMediaIcon" style="float:right;margin-top:-3%;">
+                              <li>
+                                 <div class="fb-share-button" data-layout="button_count" data-mobile-iframe="true" data-size="small">&nbsp;</div>
+                              </li>
+                              <li>
+                                 <a class="twitter-share-button" data-show-count="false" href="https://twitter.com/share">Tweet</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8">&nbsp;</script>
+                              </li>
+                              <li>
+                                 <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script><script type="IN/Share" data-counter="right"></script>&nbsp;
+                              </li>
+                              <li>
+                                 <!-- Place this tag where you want the share button to render. -->
+                                 <div class="g-plus" data-action="share" data-annotation="none">&nbsp;</div>
+                              </li>
+                              </ul>
+                           </div>
+                     </div>
+      
+                  </div>
+               </header>
+               <!-- DESKTOP POST FEATURED IMG -->
+               <div class="post-img media">
+                  <div class='mediaholder fullwidthimage'>
+                     <?php print render($content['field_home_tile_image']);?>
+                  </div>
+               </div>
+               <?php
+                  // We hide the comments and links now so that we can render them later.
+                  hide($content['comments']);
+                  hide($content['links']);
+                  print render($content['body']);
+                  ?>
+               
+               <script src="https://apis.google.com/js/platform.js" async defer></script>
+               
+               <div id="fb-root">&nbsp;</div>
+               <script>(function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+                  fjs.parentNode.insertBefore(js, fjs);
+                  }(document, 'script', 'facebook-jssdk'));
+               </script><script type="text/javascript">
+                  jQuery(document).ready(function($) {
+                     var x = $(location).attr('href');
+                     jQuery(document).ready(function($) {
+                        $('.fb-share-button').html("<a class='fb-xfbml-parse-ignore' target='_blank' href='https://www.facebook.com/sharer/sharer.php?u=" +  x  +  "&amp;src=sdkpreparse'></a>")
+                     });
+                  })  
+               </script>
+            <div id="prev-btn">
+                  <a class="go-back-button button" href="javascript:history.go(-1)">Back to previous</a>
+            </div>
+            </div>
+            <div class="block CampaignSidebar contextual-links-region region-right-sidebar col-xs-12 col-sm-12 col-md-4 col-lg-4" style="margin:0 0 50px">
+               <h3 class="headline">Most Popular</h3>
+               <span class="brd-headling"></span>
+               <div class="content">
+                  <?php echo views_embed_view('home_tile_view', 'block_6');?>
+               </div>
+            </div>
+         </section>
+      <?php else: ?>
+      <?php
+         // We hide the comments and links now so that we can render them later.
+         //redirect:
+         $outLink = "";
+         if(!is_null(render($content['field_url_for_page']))) {
+            $outLink = $content['field_url_for_page']['#items'][0]['value'];
+         } elseif(!is_null(render($content['field_external_url_for_page']))) {
+            $outLink = $content['field_external_url_for_page']['#items'][0]['value'];
+         }
+         header("Location: ".$outLink);
+         exit;
+      endif; ?>
+   <?php endif; ?>
 </div>
