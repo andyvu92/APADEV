e90db87a5f940ee718cf8f7402e6d7426f02492e
Jing Hu <Jing.Hu@australian.physio>
2020-05-12 15:55:57 +1000

    update check email existed endpoint

--- web/sites/all/modules/custom_modules/apa_function/inc/jointheAPA/jointheAPA-checkEmail.php
+++ web/sites/all/modules/custom_modules/apa_function/inc/jointheAPA/jointheAPA-checkEmail.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 
   if(isset($_POST['CheckEmailID'])){
-	//include('sites/all/themes/evolve/inc/Aptify/JSONarray.inc.php');
+  //include('sites/all/themes/evolve/inc/Aptify/JSONarray.inc.php');
+  //https://aptifywebtest.australian.physio/AptifyServicesAPI/services/VerifyUserName/
 	$postEmail['ID'] = $_POST['CheckEmailID'];
-	// create curl resource 
-	$ch = curl_init();
-    $urlcurl = "https://aptifyweb.australian.physio/AptifyServicesAPI/services/VerifyUserName/".$postEmail['ID'];	
-	// set url 
+	// create curl resource
+  $ch = curl_init();
+  $base_url = variable_get('aptify_api_base_url', APTIFY_API_BASE_URL_DEFAULT);
+  $urlcurl = $base_url ."/VerifyUserName/".$postEmail['ID'];
+	// set url
     curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_URL, $urlcurl); 
+	curl_setopt($ch, CURLOPT_URL, $urlcurl);
     curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_POSTFIELDS,null);
-	//return the transfer as a string 
+	//return the transfer as a string
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
 	curl_setopt($ch, CURLOPT_ENCODING, "");
@@ -27,7 +29,7 @@
 	}
 	curl_close($ch);
 	$result = json_decode($JSONreturn, true);
-	echo $result["response"];	
-	
+	echo $result["response"];
+
   }
 ?>
