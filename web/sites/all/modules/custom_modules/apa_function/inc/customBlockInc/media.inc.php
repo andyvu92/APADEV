3f9e33b2893216e32312364cbbbed56a40fc19eb
PHYSIOTHERAPY\jhu <Jing.Hu@physiotherapy.asn.au>
2020-10-08 15:46:13 +1100

    Added media block

---
+++ web/sites/all/modules/custom_modules/apa_function/inc/customBlockInc/media.inc.php
@@ -0,0 +1,15 @@
+<div id="section-content-top" style="height: 0">&nbsp;</div>
+<div class="media_filter">
+	<ul>
+		<li id="showall" class="active"><a>SHOW ALL</a></li>
+		<li id="InTheNews" style="cursor: pointer;"><a>IN THE NEWS</a></li>
+		<li id="mediaRelease" style="cursor: pointer;"><a>MEDIA RELEASE</a></li>
+	</ul>
+</div>
+
+<div class="media_contents_filtered">
+	<div id="downT1"><?php echo views_embed_view('media', 'block_9');?></div>
+	<div id="downT2" style="display: none;"><?php echo views_embed_view('media', 'block_10');?></div>
+	<div id="downT3" style="display: none;"><?php echo views_embed_view('media', 'block_11');?></div>
+</div>
+
