2646d2d10ed7d6b4996ddf068d65a386994f41cf
PHYSIOTHERAPY\ekim <eddy.kim@physiotherapy.asn.au>
2021-05-17 17:16:42 +1000

    CPD diary bug fix

--- web/sites/all/modules/custom_modules/apa_function/inc/CPDdiary.inc.php
+++ web/sites/all/modules/custom_modules/apa_function/inc/CPDdiary.inc.php
@@ -1,7 +1,7 @@
 <div class="space-50"></div>
+
 <?php if(isset($_SESSION['UserId'])):?>
 <?php  if($_SESSION['MemberTypeID']!="1"): ?>
-
 <?php
 //include('sites/all/themes/evolve/commonFile/updateBackgroundImage.php');
 
@@ -74,7 +74,6 @@ function date_sort($a, $b) {
 	return $c;
 }
 ?>
-
 <div id="CPD-diary-main">
 <div class="container">
 <div class="row">
@@ -124,9 +123,8 @@ function date_sort($a, $b) {
 		</div>
 	</div>
 </div>
-
 <div class="TabContents2" style="display: none;">
-<!--strong><a href="http://www.physiotherapyboard.gov.au/documents/default.aspx?record=WD15%2f18489&dbid=AP&chksum=ewqLtzOm4m%2fsRUrlGCmo1A%3d%3d">This is based on Physiotherapy Board of Australia's Continuing professional development form.</a></strong-->
+<?php /*<!--strong><a href="http://www.physiotherapyboard.gov.au/documents/default.aspx?record=WD15%2f18489&dbid=AP&chksum=ewqLtzOm4m%2fsRUrlGCmo1A%3d%3d">This is based on Physiotherapy Board of Australia's Continuing professional development form.</a></strong-->*/ ?>
 <div class="NAPAhours">
   <div class="NAPAhoursHead flex-cell flex-flow-row heading-row">
     <div class="flex-col-4 table-heading ">Description</div><div class="flex-col-1 table-heading date">Date</div><div class="flex-col-1 table-heading hours">Hours</div><div class="flex-col-2 table-heading ">Provider</div><div class="flex-col-4 table-heading reflection">Reflection</div>
@@ -139,7 +137,9 @@ function date_sort($a, $b) {
 			echo "<div class='flex-cell flex-flow-row'>";
 			$dateCPD = str_replace('/', '-', $rowData["Date"]);//$rowData["Date"];//
 			$date = date("d/m/Y", strtotime($dateCPD));
-			echo "<div style='display: none;'>".$rowData["NPDid"]."</div><div class='flex-col-4'><b><span class='mobile-header'>Description</span>".$rowData["Description"]."</b></div><div class='flex-col-1 date'><span class='mobile-header'>Date</span>".$date."</div><div class='flex-col-1 hours'><span class='mobile-header'>Hours</span>".$rowData["Time"]."</div><div class='flex-col-2'><span class='mobile-header'>Provider</span>".$rowData["Provider"]."</div><div class='flex-col-4 reflection'><span class='mobile-header'>Reflection</span>".$rowData["Reflection"]."</div>";
+			$ReflectionFiltered = str_replace('<', '', $rowData["Reflection"]);
+			$ReflectionFiltered = str_replace('>', '', $ReflectionFiltered);
+			echo "<div style='display: none;'>".$rowData["NPDid"]."</div><div class='flex-col-4'><b><span class='mobile-header'>Description</span>".$rowData["Description"]."</b></div><div class='flex-col-1 date'><span class='mobile-header'>Date</span>".$date."</div><div class='flex-col-1 hours'><span class='mobile-header'>Hours</span>".$rowData["Time"]."</div><div class='flex-col-2'><span class='mobile-header'>Provider</span>".$rowData["Provider"]."</div><div class='flex-col-4 reflection'><span class='mobile-header'>Reflection</span>".$ReflectionFiltered."</div>";
 			echo "<div class='lineBreak'>&nbsp;</div>";
 			echo "</div>";
 			$arrT["ID"] = $rowData["NPDid"];
@@ -147,7 +147,7 @@ function date_sort($a, $b) {
 			$arrT["Date"] = $rowData["Date"];
 			$arrT["Hours"] = $rowData["Time"];
 			$arrT["Provider"] = $rowData["Provider"];
-			$arrT["Reflection"] = $rowData["Reflection"];
+			$arrT["Reflection"] = $ReflectionFiltered;
 			array_push($DiaryAll, $arrT);
 		}
 	}else {
@@ -182,7 +182,6 @@ function date_sort($a, $b) {
 	</div>
 	</div>
 </div>
-
 </div>
 </div></div>
 <!--div class="container"-->
@@ -267,194 +266,3 @@ function date_sort($a, $b) {
 
 		</div>
 <?php endif;?>
-
-<?php
-// 2.2.2 - GET Membership certification PDF
-// Send -
-// UserID
-// Response -
-// Membership certificate PDF
-//echo "<iframe name='YInkFroamame' src='http://www.physiotherapy.asn.au'></iframe>";
-// $Certi = aptify_get_GetAptifyData("38", $_SESSION['LinkId']);
-//echo "<iframe id="YInkFroamame" srcdoc='".$MemberCerti."'></iframe>";
-//echo "<iframe src='".$MemberCerti."'></iframe>";
-//echo "<iframe src='https://apaaptifywebuat.aptify.com/AptifyServicesAPI/services/Authentication/Login/DomainWithContainer?UserName=aptifyuser&Password=!@-auser-Apatest1-2468' style='display: none;'></iframe>";
-//echo "<iframe src='https://apaaptifywebuat.aptify.com/AptifyServicesAPI/forms/CrystalReportView.aspx?ViewMode=entityRecord&amp;ViewEntityName=Persons&amp;ReportId=151&amp;EntityRecordID=55280'width='900' height='900'></iframe>";
-//echo "<a href='https://apaaptifywebuat.aptify.com/AptifyServicesAPI/forms/CrystalReportView.aspx?ViewMode=entityRecord&ViewEntityName=Persons&ReportId=151&EntityRecordID=".$_SESSION['LinkId']."'>ttttt</a>";
-?>
-<?php
-/*
-<input type="submit" class="Non-APA-hour" name="NONAPA" id="NONAPA" value="Download NON-APA PDF">
-<button type="button" class="btn btn-info btn-lg Non-APA-hour" data-toggle="modal" data-target="#NonCPDPDF"><span style="text-decoration: underline; color:white;">Download NON-APA PDF</span></button>
-*/
-?>
-<?php /*
-<div id="NonCPDPDF" class="modal fade big-screen" role="dialog">
-	<div class="modal-dialog">
-
-	<!-- Modal content-->
-	<div class="modal-content">
-		<div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal">&times;</button>
-		</div>
-		<div class="modal-body">
-		<iframe name='NonCPDPDF' src='http://www.physiotherapy.asn.au'></iframe>
-		</div>
-		<div class="modal-footer">
-		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-		</div>
-	</div>
-
-	</div>
-</div>
-<script>
-$(document).ready(function() {
-	if (window.frames['NonCPDPDF'] && !window.userSet){
-		window.userSet = true;
-		frames['NonCPDPDF'].location.href='<?php echo $Certi; ?>';
-	}
-});
-</script>
-*/
-?>
-<?php
-/*
-echo "Result: <br />";
-echo $results;
-echo "<br /><br />Result var dump: <br />";
-var_dump($results);
-echo "<br /><br />cpdhours: ".$CPDHousrs;
-echo "<br /><br />apa <br />";
-print_r($APA);
-echo "<br /><br />nonpap <br />";
-print_r($NAPA);
-
-
-
-<script type="text/javascript">
-	function changeStatus(number){
-		document.formradio.background[number].checked=true;
-	}
-
-	function myFunction() {
-		var background_number =document.formradio.background.value;
-		var pre_bg =  document.getElementById('pre_background').innerHTML;
-		document.getElementById("CPD-diary-main").classList.remove(pre_bg);
-		var cur_bg = 'background_' +background_number;
-		document.getElementById("CPD-diary-main").classList.add(cur_bg);
-		document.getElementById('pre_background').innerHTML = 'background_' + background_number;
-}
-</script>
-
-<div id="pre_background" style="display:none">background_2</div>
-<div id="CPD-diary-main" class="background_<?php echo $background; ?>">
-<div class="container">
-<button class="dashboard-backgroud" data-toggle="modal" data-target="#myModal"><span class="customise_background" >Customise your background</span><span class="customise_icon">[icon class="fa fa-cogs fa-x"][/icon]</span></button>
-</div>
-<div class="container">
-    <!-- Modal -->
-	<div id="myModal" class="modal fade" role="dialog">
-	<div class="modal-dialog" style="overflow-y: scroll; max-height:85%;  margin-top: 50px; margin-bottom:50px;">
-		<!-- Modal content-->
-		<div class="modal-content">
-		<div class="modal-header">
-			<button type="button" class="close" data-dismiss="modal">&times;</button>
-			<h4 class="modal-title">Please select background image</h4>
-		</div>
-		<div class="modal-body">
-			<form name="formradio" action="cpd-diary" method="POST">
-			<input type="hidden" name="userID" value="<?php echo $userID; ?>">
-			<input type="hidden" name="update">
-			<label> <input type="radio" name="background" value="1" id="background1"><img src="/sites/default/files/PARALLAX_TRADIES.jpg"></label>
-			<label> <input type="radio" name="background" value="2"  id="background2"><img src="/sites/default/files/ABOUT-US/ABOUTUS1170X600.jpg"></label>
-			<label> <input type="radio" name="background" value="3"  id="background3"><img src="/sites/default/files/NG_1200X600_RURAL.png"></label>
-			<label> <input type="radio" name="background" value="4"  id="background4"><img src="/sites/default/files/MEDIA1170X600_5.jpg"></label>
-		</div>
-		<div class="modal-footer">
-			<button type="Submit" class="btn btn-default" id="background_save">Save</button>
-			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-		</div>
-		</form>
-		</div>
-	</div>
-	</div>
-
-	<div id="myProgress">
-	<div id="currentProgress">0 hours currently</div>
-
-	<div id="ProgressCPDback">
-		<span class="front fa fa-circle"></span>
-		<span class="back fa fa-circle"></span>
-	</div>
-	<div id="myBar">10%</div>
-
-	<div id="MaxHours">
-	<div class="trophy"><i class="fa fa-trophy fa-2x"></i></div>
-
-	<div class="MaxHours">20 hours minimum</div>
-	</div>
-	</div>
-</div>
-
-<form><input id="inputProgress" type="text" /> <input id="buttonProgress" type="button" value="Click Me" />&nbsp;</form>
-</div>
-
-<div class='lineBreak' style="height: 40px;">&nbsp;</div>
-
-<script>
-  jQuery(document).ready(function($) {
-	$('#buttonProgress').click(function() {
-      var thiseee = $('#inputProgress').val();
-		move(thiseee);
-	});
-  });
-</script><script>
-
-document.onload = move(<?php echo $CPDHousrs; ?>);
-function move(input) {
-  var elem = document.getElementById("myBar");
-  var pointy = document.getElementById("currentProgress");
-  var totalWidth = document.getElementById("myProgress").offsetWidth;
-  var maxHour = document.getElementById("MaxHours");
-  var width = 0;
-  var CurrentHrs = 0;
-  var termMax = 20;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (width > 100) { // to stop the progress bar
-      clearInterval(id);
-    } else if(CurrentHrs >= termMax) { // when exceeded the limit
-    	elem.style.width = width+'%';
-        elem.innerHTML = CurrentHrs+'/'+termMax;
-        pointy.style.marginLeft = (totalWidth - 37)+ 'px';
-        currentProgress.innerHTML = CurrentHrs + ' hours currently';
-        width++;
-        if(CurrentHrs < input) {
-        	CurrentHrs++;
-        }
-    } else { // in progress
-      if(input <= termMax) {
-      	CurrentHrs = 0.25 + CurrentHrs;
-      	width = Math.floor(CurrentHrs/termMax * 100);
-      	elem.style.width = width + '%';
-      	elem.innerHTML = width + '%';
-      	pointy.style.marginLeft = ((totalWidth * width / 100) - 36) + 'px';
-      	currentProgress.innerHTML = Math.floor(CurrentHrs) + ' hours currently';
-        maxHour.style.paddingLeft = totalWidth+'px';
-        if(CurrentHrs >= input) clearInterval(id);
-      } else {
-      	CurrentHrs++;
-      	width++;
-      	elem.style.width = width + '%';
-      	elem.innerHTML = width * 1  + '%';
-      	pointy.style.marginLeft = ((width * 4)/5) + '%';
-      	currentProgress.innerHTML = CurrentHrs + ' hours currently';
-        maxHour.style.paddingLeft = (width/input*100) + '%';
-      }
-    }
-  }
-}
-</script>
-
-*/
-?>
